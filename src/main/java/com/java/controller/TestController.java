892bd5b0d717698ed18569fe999a7b966aaca2bc
JosephBean <kk1593@naver.com>
2025-01-06 16:07:17 +0900

    findAll() & save()

--- src/main/java/com/java/controller/TestController.java
+++ src/main/java/com/java/controller/TestController.java
@@ -1,21 +1,36 @@
 package com.java.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.java.domain.TestEntity;
 import com.java.repository.TestEntityRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-@RestController
+@Controller
 public class TestController {
 
 	private final TestEntityRepository testEntityRepository;
 	
 	@GetMapping("/test")
-	public String test() {
-		return "OK";
+	public String test(Model model) {
+		model.addAttribute("result", testEntityRepository.findAll());
+		return "test";
+	}
+	
+	@PostMapping("/save")
+	public String save(@RequestParam("txt") String txt) {
+		TestEntity tEntity = new TestEntity();
+		tEntity.setTxt(txt);
+		
+		testEntityRepository.save(tEntity);
+		
+		return "redirect:/test";
 	}
 	
 }
--- src/main/java/com/java/domain/TestEntity.java
+++ src/main/java/com/java/domain/TestEntity.java
@@ -1,5 +1,6 @@
 package com.java.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,6 +23,7 @@ public class TestEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int no;
 	
+	@Column(length = 50, nullable = false)
 	private String txt;
 	
 }
